5bf06c71eb146ce61f18cab049f4460a910c5d0f
MWERGEN <108718663+MWERGEN@users.noreply.github.com>
2023-04-07 09:42:19 +0200

    added file header + test print

---
+++ main.py
@@ -0,0 +1,18 @@
+#   Created for BTree-MTT on 07.04.23
+#
+#   Creator: Marius Wergen
+#
+#   Task: 
+#
+#   Editors:
+#       1.  Marius Wergen on 07.04.23
+#       2.
+#
+###############################################
+#
+#   File description:
+#       - main file
+#
+
+
+print("Gumo")
