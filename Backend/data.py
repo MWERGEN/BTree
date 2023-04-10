a4f54bd9d38c2359ae37de2fd3714087213dc044
TimSt <tim.steiner01@web.de>
2023-04-10 17:52:01 +0200

    initial class creation
    Created all classes according to our architecture.

---
+++ Backend/data.py
@@ -0,0 +1,15 @@
+#   Created for BTree-MTT on 10.04.23
+#
+#   Creator: Tim Steiner
+#
+#   Task: 
+#
+#   Editors:
+#       1.  Tim Steiner on 10.04.23
+#       2.
+#
+###############################################
+#
+#   File description:
+#       - user data from the GUI
+#
---
+++ Frontend/bTreeVisualization.py
@@ -0,0 +1,15 @@
+#   Created for BTree-MTT on 10.04.23
+#
+#   Creator: Tim Steiner
+#
+#   Task: 
+#
+#   Editors:
+#       1.  Tim Steiner on 10.04.23
+#       2.
+#
+###############################################
+#
+#   File description:
+#       - visualization of the B-tree
+#
