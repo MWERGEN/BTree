33b1050392c0f448386c2c66e9592f97ca484e31
MWERGEN <108718663+MWERGEN@users.noreply.github.com>
2023-05-03 09:57:54 +0200

    edges logic ready

--- Frontend/gui.py
+++ Frontend/gui.py
@@ -6,7 +6,7 @@
 #
 #   Editors:
 #       1.  Tim Steiner on 10.04.23
-#       2.
+#       2.  Marius Wergen on 03.05.23
 #
 ###############################################
 #
@@ -20,7 +20,7 @@ class Gui:
     # function for testing draw-behaviour of graph
     def drawGraph(self):
             # btree Visualization object (k = 3, key-width = 0.06, ref-width = 0.03, minNodeDistance = 0.1, nodesList[lowest level, ... , root], keysList)
-            self.GraphNodes = bt.BTreeVisualization(3, 0.06, 0.03, 0.1, [6, 3, 1], [[1, 2], [7, 8, 9], [15], [33, 40], [50, 69, 70], [100], [4, 12], [41], [75, 76], [20]])
+            self.GraphNodes = bt.BTreeVisualization(3, 0.06, 0.03, 0.1, [6, 3, 1], [[1, 2], [7, 8, 9], [15], [33, 40], [50, 69, 70], [100], [4, 12], [41], [75, 76], [20]], [[], [], [], [], [], [], [0, 1, 2], [3, 4], [5], [6, 7, 8]])
             # predefine 4 nodes 
             self.GraphNodes.calcNodesPositions()
             # calc where the references will be inside the nodes
@@ -29,6 +29,8 @@ class Gui:
             self.GraphNodes.calcKeyPositions()
             # calculate all graphs
             self.GraphNodes.assertValuesToGraphs()
+            # calculate the edges' relationships
+            self.GraphNodes.calcEdges()
             # draw all graphs
             # TODO: animation 
             self.GraphNodes.runAnimation()
