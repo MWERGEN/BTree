6f3bc993127a2cf36e29313702d631ec66e5429a
TimSt <tim.steiner01@web.de>
2023-05-07 16:34:25 +0200

    animation list for frontend

--- Backend/data.py
+++ Backend/data.py
@@ -19,8 +19,28 @@ from bTree import BTree
 # tree with at least 3 keys and max 6 keys in each node!
 testTree = BTree(2)
 
-for i in range(10):
-    testTree.insertKey(i*3)
+#for i in range(20):
+#    testTree.insertKey(i*3)
+
+testTree.insertKey(5)
+testTree.insertKey(7)
+testTree.insertKey(2)
+testTree.insertKey(8)
+testTree.insertKey(0)
+testTree.insertKey(3)
+testTree.insertKey(11)
+testTree.insertKey(58)
+testTree.insertKey(44)
+testTree.insertKey(32)
+testTree.insertKey(96)
+testTree.insertKey(8)
+testTree.insertKey(9)
+testTree.insertKey(10)
+testTree.insertKey(103)
+testTree.insertKey(6)
+testTree.insertKey(200)
+testTree.insertKey(203)
+testTree.insertKey(14)
 
 
 testTree.printTree(testTree.rootNode)
